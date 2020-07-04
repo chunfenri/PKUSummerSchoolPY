39cfa0b37351b3df84ea4135880a05eb15d05d60
chunfenri <yangliming2018@pku.edu.cn>
2020-07-04 18:01:38 +0800

    new

---
+++ main.py
@@ -0,0 +1,36 @@
+import pgzero
+import math
+import random
+
+BubbleColor = ['red', 'yellow', 'green', 'blue']
+
+epoch = 0
+
+
+class bubble():
+    color = random.choice(BubbleColor)
+    posx = 0
+    posy = 0
+
+
+def init():
+    pass
+
+
+def traceBubble():
+    pass
+
+def BubbleHitPosition(bubble):
+    pass
+
+def judgeBubbleBoom(bubble):
+    pass
+
+def bubbleExplode(bubbleList):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
