abf52b6c2d1ab1abbfdf635451058ca464113786
chunfenri <yangliming2018@pku.edu.cn>
2020-07-08 18:17:40 +0800

    new

--- main.py
+++ main.py
@@ -5,23 +5,43 @@ import random
 
 WIDTH = 600
 HEIGHT = 1200
+newBubbleIdx = 1
+
+bubbleColor = ['red', 'yellow', 'green', 'blue']
+
+bubblePos = [[] for i in range(20)]
+
+activeBubble = {}
 
 
-BubbleColor = ['red', 'yellow', 'green', 'blue']
+print(bubblePos)
 
 epoch = 0
 
+
 def draw():
     screen.clear()
-    screen.fill((128,0,0))
+    screen.fill((128, 0, 0))
 
+
+def update():
+    pass
+
+
+def on_mouse_down(pos):
+    pass
 
 
 class bubble():
-    color = random.choice(BubbleColor)
-    posx = 0
-    posy = 0
+    def __init__(self, x, y):
+        self.color = random.choice(bubbleColor)
+        self.posx = x
+        self.posy = y
+        self.index = newBubbleIdx
+        newBubbleIdx += 1
+        activeBubble[self.index] = self
 
+    
 
 
 def init():
@@ -31,15 +51,18 @@ def init():
 def traceBubble():
     pass
 
+
 def BubbleHitPosition(bubble):
     pass
 
+
 def judgeBubbleBoom(bubble):
     pass
+
 
 def bubbleExplode(bubbleList):
     pass
 
 
-sounds.background1.play()
+sounds.background3.play()
 pgzrun.go()
