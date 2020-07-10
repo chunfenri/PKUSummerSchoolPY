e79c05e4cbf68890017f7c1651372bafb944c659
zwh0211 <zwh0211@outlook.com>
2020-07-10 22:55:09 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -4,48 +4,48 @@ import math
 import random
 '''数组0，0是左上角，往下一个是1，0；往右一个是0，1.要求是第一次第一行有10个，下一行9个
 下一行10个，下一行9个，呈现六边形密堆积，其中10个球的直径正好填满横排'''
-'''右下角下标为(22,9)'''
+'''右下角下标为(9,22)'''
 
 
 def index2pos(indx, indy):
     rad = 30
-    dx = rad * math.sqrt(3)
-    x_pixel = rad + dx * indx
-    if indx % 2 == 0:
-        y_pixel = rad + 2 * rad * indy
+    dy = rad * math.sqrt(3)
+    y_pixel = rad + dy * indy
+    if indy % 2 == 0:
+        x_pixel = rad + 2 * rad * indx
     else:
-        y_pixel = 2 * rad + 2 * rad * indy
-    posx = x_pixel / 1200
-    posy = y_pixel / 600
+        x_pixel = 2 * rad + 2 * rad * indx
+    posy = y_pixel / 1200
+    posx = x_pixel / 600
     return (posx, posy)
 
 
 def index2pos_pixel(indx, indy):
     rad = 30
-    dx = rad * math.sqrt(3)
-    x_pixel = rad + dx * indx
-    if indx % 2 == 0:
-        y_pixel = rad + 2 * rad * indy
+    dy = rad * math.sqrt(3)
+    y_pixel = rad + dy * indy
+    if indy % 2 == 0:
+        x_pixel = rad + 2 * rad * indx
     else:
-        y_pixel = 2 * rad + 2 * rad * indy
+        x_pixel = 2 * rad + 2 * rad * indx
     return (x_pixel, y_pixel)
 
 
-def pos2index(posx, posy):
+def pos2index(posy, posx):
     '''主要用于bubble碰撞到别的bubble时看应该要塞到哪个位置，位置可能不是准确位置，在一个范围内都要映射到一个位置'''
     rad = 30
-    dx = rad * math.sqrt(3)
-    x_pixel = posx * 1200
-    y_pixel = posy * 600
-    indx_esm = [
-        int(x_pixel / dx) - 1,
-        int(x_pixel / dx),
-        int(x_pixel / dx) + 1
-    ]
+    dy = rad * math.sqrt(3)
+    y_pixel = posy * 1200
+    x_pixel = posx * 600
     indy_esm = [
-        int(y_pixel / (2 * rad)) - 1,
-        int(y_pixel / (2 * rad)),
-        int(y_pixel / (2 * rad)) + 1
+        int(y_pixel / dy) - 1,
+        int(y_pixel / dy),
+        int(y_pixel / dy) + 1
+    ]
+    indx_esm = [
+        int(x_pixel / (2 * rad)) - 1,
+        int(x_pixel / (2 * rad)),
+        int(x_pixel / (2 * rad)) + 1
     ]
     for x in indx_esm:
         for y in indy_esm:
@@ -58,17 +58,17 @@ def pos2index(posx, posy):
 
 
 def judgeConnect(bubbleA, bubbleB):
-    indxA, indyA = bubbleA.indx, bubbleA.indy
-    indxB, indyB = bubbleB.indx, bubbleB.indy
-    if indxA == indxB:
-        if abs(indyA - indyB) == 1:
+    A_indx, A_indy = bubbleA.indx, bubbleA.indy
+    B_indx, B_indy = bubbleB.indx, bubbleB.indy
+    if A_indy == B_indy:
+        if abs A_indx - B_indx) == 1:
             return 1
-    elif abs(indxA - indxB) == 1:
-        if indxA % 2 == 0:
-            if indyA == indyB or indyA - 1 == indyB:
+    elif abs(A_indy - B_indy) == 1:
+        if A_indy % 2 == 0:
+            if A_indx == B_indx or A_indx - 1 == B_indx:
                 return 1
         else:
-            if indyA == indyB or indyA + 1 == indyB:
+            if A_indx == B_indx or A_indx + 1 == B_indx:
                 return 1
 
 
