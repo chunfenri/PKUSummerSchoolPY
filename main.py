241c3315bef74647f805280303f04c768f36e54d
chunfenri <yangliming2018@pku.edu.cn>
2020-07-10 22:44:43 +0800

    new

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
 import pgzrun
-import pgzero
+import pgzero as pg
 import math
 import random
+import time
 
 WIDTH = 600
 HEIGHT = 1200
-newBubbleIdx = 1
 
-bubbleColor = ['red', 'yellow', 'green', 'blue']
 
-bubblePos = [[0 for _ in range(10)] for _ in range(20)]
+bubbleColor = ['red', 'yellow', 'green', 'dblue']
+
+bubblePos = [["" for _ in range(10)] for _ in range(20)]
 
 activeBubble = {}
 
-
-print(bubblePos)
-
-epoch = 0
-
-
-def draw():
-    screen.clear()
-    screen.fill((128, 0, 0))
-
-
-def update():
-    pass
-
-
-
-def on_mouse_down(pos):
-    pass
+movingLine = False
 
 
 class bubble():
@@ -38,10 +22,34 @@ class bubble():
         self.color = random.choice(bubbleColor)
         self.indx = x
         self.indy = y
-        self.index = newBubbleIdx
-        newBubbleIdx += 1
-        activeBubble[self.index] = self
-        bubblePos[y][x] = self.index
+        self.pic = Actor(self.color)
+
+        activeBubble[(y, x)] = self
+        bubblePos[y][x] = self.color
+
+
+def index2pos(indx, indy):
+    rad = 30
+    dx = rad * math.sqrt(3)
+    x_pixel = rad + dx * indx
+    if indx % 2 == 0:
+        y_pixel = rad + 2 * rad * indy
+    else:
+        y_pixel = 2 * rad + 2 * rad * indy
+    posx = x_pixel / 1200
+    posy = y_pixel / 600
+    return (posx, posy)
+
+
+def index2pos_pixel(indx, indy):
+    rad = 30
+    dx = rad * math.sqrt(3)
+    x_pixel = rad + dx * indx
+    if indx % 2 == 0:
+        y_pixel = rad + 2 * rad * indy
+    else:
+        y_pixel = 2 * rad + 2 * rad * indy
+    return (x_pixel, y_pixel)
 
 
 def init():
@@ -64,5 +72,66 @@ def bubbleExplode(bubbleList):
     pass
 
 
+def generateLine():
+    for i in bubblePos[19]:
+        if i != '':
+            return False
+
+    for i in range(19):
+        for j in range(10):
+            bubblePos[i + 1][j] = bubblePos[i][j]
+
+    key, value = [], []
+    for keys in activeBubble:
+        newkey = list(key)
+        newkey[0] += 1
+        newkey = tuple(newkey)
+        key.append(newkey)
+        value.append(activeBubble[keys])
+
+    activeBubble.clear()
+    activeBubble.fromkeys(key, value)
+
+    global epoch
+    if epoch % 2:
+        lineNum = 9
+    else:
+        lineNum = 10
+    epoch += 1
+
+    for i in range(lineNum):
+        bubble(i, 0)
+    return True
+
+
+def draw():
+    screen.clear()
+    screen.fill((128, 0, 0))
+    actor.draw()
+    for bubble in activeBubble.values():
+        bubble.pic.draw()
+
+    if(time.time()-startTime < 1):
+        global epoch
+        print(bubblePos, epoch)
+        print(activeBubble)
+        epoch += 1
+
+
+def update():
+    global movingLine
+    if movingLine:
+        for bubbles in activeBubble.values():
+            
+
+
+def on_mouse_down(pos):
+    pass
+
+
+epoch = 0
 sounds.background3.play()
+a = bubble(0, 0)
+a.pic.topleft = index2pos(0, 0)
+startTime = time.time()
 pgzrun.go()
